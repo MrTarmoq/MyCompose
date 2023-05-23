8d115b37a8ca7ecfbc081b11948b24c57ecb987b
omonov <omonovshohjahon77@gmail.com>
2023-05-23 15:36:10 +0500

    Trying to commit and push first time in Linux

--- app/src/main/java/uz/gita/mr_smart/mycompose/MainActivity.kt
+++ app/src/main/java/uz/gita/mr_smart/mycompose/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyComposeTheme {
-        Greeting("ggfa")
+        Greeting("ggfasdssd")
     }
 }
